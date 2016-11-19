198390ce396c4ea05ebadeb298b1030663b3ca9d
Mikhail Kelner <mikhail.kelner@gmail.com>
2016-11-19 19:40:47 +0200

    Possibility to use XSRF protection for some other purposes

--- src/Orange/Forms/XSRFProtection.php
+++ src/Orange/Forms/XSRFProtection.php
@@ -30,20 +30,22 @@ class XSRFProtection
     private function __construct(){
     }
 
-    public function key($args = [], $time = null){
+    public function key($args = [], $time = null, $ignore_basic_args = false){
         if (is_null($time)){
             $time = time();
         }
         $key_args = [gmdate("YmdH", $time)];
-        $key_args = array_merge($key_args, static::$unique_components);
+        if (!$ignore_basic_args) {
+            $key_args = array_merge($key_args, static::$unique_components);
+        }
         $key_args = array_merge($key_args, $args);
         return md5(implode(':', $key_args));
     }
 
-    public function check($key, $args = []){
+    public function check($key, $args = [], $ignore_basic_args = false){
         $result = false;
         for ($i = 0; $i < static::$key_life_hours; $i++){
-            if ($key === $this->key($args, time() - $i * 3600)){
+            if ($key === $this->key($args, time() - $i * 3600, $ignore_basic_args)){
                 $result = true;
                 break;
             }
